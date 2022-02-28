6fdf2f945a7030b08436339742bb4533b1469b24
Juan Toro <jct20xx@gmail.com>
2022-02-27 20:23:51 -0500

    commit tree 3e48302fd01d81bb97675005ff63a0f1285bc396

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function saturdayFun (activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`);
+};
+
+const workFromHome = function (location){
+    return (`This Monday, I will ${location}.`);
+};
+
+function mondayWork (location = "go to the office") {
+   return workFromHome (location);
+};
+
+
+const wrapAdjective = function (wrap = "*"){
+    return function (occupation = "special"){
+        return `You are ${wrap}${occupation}${wrap}!`;
+    }
+};
